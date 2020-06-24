c150167e16b5e14b17c35d2f182e85f07e41192e
Tommaso Giubilei <tms.gbl@gmail.com>
2020-06-24 12:27:21 +0200

    secondCommit
    fsd

--- Script/menuscript.js
+++
@@ -1,9 +0,0 @@
-function loadCSS(filename){ 
-
-    var file = document.createElement("link");
-    file.setAttribute("rel", "stylesheet");
-    file.setAttribute("type", "text/css");
-    file.setAttribute("href", filename);
-    document.head.appendChild(file);
-
- }
