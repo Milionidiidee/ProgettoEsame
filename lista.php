2671015e1f9b3b37ddf89608a2ffd7e418aa5b9f
Tommaso Giubilei <tms.gbl@gmail.com>
2020-06-25 11:46:19 +0200

    Filtro funzionante
    fsda

--- lista.php
+++ lista.php
@@ -10,16 +10,24 @@
     try {
         $conn = new PDO("mysql:host=localhost; dbname=phpmyadmin","ciao","ciao");
 
-        $sql = "SELECT * FROM prodotti WHERE Nome='Noninf'";
+        $sql = "SELECT * FROM prodotti";
+
+        if (isset($_POST['nome']) && $_POST['nome'] != "") {
+            $sql = $sql." WHERE Nome LIKE '%".$_POST['nome']."%'";
+            echo $sql;
+            echo "<br>";
+        }
 
         $q = $conn->query($sql);
-        #$q-> setFetchMode(PDO::FETCH_ASSOC);
 
-        $rows = $q->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach( $rows as $row )
-        {
-            echo "Nome: " . $row["Nome"]. " - Img: " . $row["Immagine"]. " - Desc: " . $row["Descrizione"]. " - Prezzo: " . $row["Prezzo"]. "<br>";
+        if ($q) {
+            $rows = $q->fetchAll(PDO::FETCH_ASSOC);
+            foreach( $rows as $row )
+            {
+                echo "Nome: " . $row["Nome"]. " - Img: " . $row["Immagine"]. " - Desc: " . $row["Descrizione"]. " - Prezzo: " . $row["Prezzo"]. "<br>";
+            }
+        }else {
+            echo "Err";
         }
     }
     catch (Exception $e) {
@@ -27,17 +35,21 @@
     }
 
     #per ogni elemento nell'array del Db creo la casella prodotto
-    echo "    <div>
-    <img src='img'>
-    <h3>Nome</h3>
-    <img src='rating'>
-    <h5>prezzo</h5>
-    </div>";
     ?>
+
+    <h2 id="ricerca">Ricerca Avanzata</h2>
+        <form action="./lista.php" method="POST">
+            <label for="nome">Nome: </label><br>
+            <input name="nome" id="nome" type="text" value=""><br>
+            <label for="categoria">Categoria: </label><br>
+                <select name="categoria" id="categoria">
+                    <option value="vuoto" selected> </option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                </select><br>
+                <input type="submit" value="Cerca">
+        </form>
+
+    <script src="public/app.js"></script>
 </body>
 </html>
-
-<?php
-while ($row = $q->fetch()){
-            echo "Nome: " . $row["Nome"]. " - Img: " . $row["Immagine"]. " - Desc: " . $row["Descrizione"]. " - Prezzo: " . $row["Prezzo"]. "<br>";
-        } ?>
